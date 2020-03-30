fcc727a1c29aac2abc6b50d9e521ae3057597700
ChenXiangGit <985699062@qq.com>
2020-03-30 11:35:08 +0800

    1

---
+++ SpringBoot/并发/demo/src/main/java/org/example/demo/FutureTaskDemo.java
@@ -0,0 +1,39 @@
+package org.example.demo;
+
+import java.util.concurrent.*;
+
+/**
+ * @ClassName: FutureTaskDemo
+ * @Description:
+ * @Author: Chen
+ * @Date: 2020/3/30 10:25
+ * @Version: 1.0
+ */
+public class FutureTaskDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        FutureTask<String> ft1 = new FutureTask<>(() -> {
+            System.out.println("啦啦啦");
+//            Thread.sleep(2000);
+//            return "1";
+        },"987");//指定返回结果，线程任务执行成功，返回987
+        FutureTask<String> ft2 = new FutureTask<>(() -> {
+            Thread.sleep(1000);
+            return "2";
+        });
+        FutureTask<String> ft3 = new FutureTask<>(() -> "3");
+
+        executorService.submit(ft1);
+        executorService.submit(ft2);
+        executorService.submit(ft3);
+
+        System.out.println( ft1.get());
+        System.out.println( ft2.get());
+        System.out.println( ft3.get());
+
+        executorService.shutdown();
+        System.out.println("shut down");
+    }
+}
